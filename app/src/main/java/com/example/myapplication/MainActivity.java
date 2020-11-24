715036223be9948f7d119e28cea9a371b0c364ed
tal zaidman <Talzaidman5@gmail.com>
2020-11-24 20:40:36 +0200

    after pull

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,29 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayList<String>a = new ArrayList<>();
-        a.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKmJpelOZfVhNJN9Q86jPcFKTogPLytfKEug&usqp=CAU");
-        a.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKmJpelOZfVhNJN9Q86jPcFKTogPLytfKEug&usqp=CAU");
-        a.add("https://www.travelanddestinations.com/wp-content/uploads/2019/10/Ban-Gioc-Detian-Waterfalls-Vietnam-1.jpg");
-        a.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJFh0J4U_hIVCAelRvwTEDdjZEPibbffFBzA&usqp=CAU");
-        a.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJFh0J4U_hIVCAelRvwTEDdjZEPibbffFBzA&usqp=CAU");
-        a.add("https://q-xx.bstatic.com/xdata/images/hotel/840x460/78809294.jpg?k=cf850d507a9671cf7ff85d598435ea329a28cd4f1b1abc25c1892c91156d36ad&o=");
-        a.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJFh0J4U_hIVCAelRvwTEDdjZEPibbffFBzA&usqp=CAU");
-        a.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ722TbS-z3Yf3Eel0FVfa31C-IH6qBcIuI8w&usqp=CAU");
-        a.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8IrJPigxJGRwCJhrQ8I_AGrekKouB51RL6A&usqp=CAU");
-        a.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8IrJPigxJGRwCJhrQ8I_AGrekKouB51RL6A&usqp=CAU");
-        a.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8IrJPigxJGRwCJhrQ8I_AGrekKouB51RL6A&usqp=CAU");
-        a.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8IrJPigxJGRwCJhrQ8I_AGrekKouB51RL6A&usqp=CAU");
-        a.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKmJpelOZfVhNJN9Q86jPcFKTogPLytfKEug&usqp=CAU");
-        a.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKmJpelOZfVhNJN9Q86jPcFKTogPLytfKEug&usqp=CAU");
+        ArrayList<String>allPhotos = new ArrayList<>();
+        allPhotos.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKmJpelOZfVhNJN9Q86jPcFKTogPLytfKEug&usqp=CAU");
+        allPhotos.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKmJpelOZfVhNJN9Q86jPcFKTogPLytfKEug&usqp=CAU");
+        allPhotos.add("https://www.travelanddestinations.com/wp-content/uploads/2019/10/Ban-Gioc-Detian-Waterfalls-Vietnam-1.jpg");
+        allPhotos.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJFh0J4U_hIVCAelRvwTEDdjZEPibbffFBzA&usqp=CAU");
+        allPhotos.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJFh0J4U_hIVCAelRvwTEDdjZEPibbffFBzA&usqp=CAU");
+        allPhotos.add("https://q-xx.bstatic.com/xdata/images/hotel/840x460/78809294.jpg?k=cf850d507a9671cf7ff85d598435ea329a28cd4f1b1abc25c1892c91156d36ad&o=");
+        allPhotos.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJFh0J4U_hIVCAelRvwTEDdjZEPibbffFBzA&usqp=CAU");
+        allPhotos.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ722TbS-z3Yf3Eel0FVfa31C-IH6qBcIuI8w&usqp=CAU");
+        allPhotos.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8IrJPigxJGRwCJhrQ8I_AGrekKouB51RL6A&usqp=CAU");
+        allPhotos.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8IrJPigxJGRwCJhrQ8I_AGrekKouB51RL6A&usqp=CAU");
+        allPhotos.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8IrJPigxJGRwCJhrQ8I_AGrekKouB51RL6A&usqp=CAU");
+        allPhotos.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8IrJPigxJGRwCJhrQ8I_AGrekKouB51RL6A&usqp=CAU");
+        allPhotos.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKmJpelOZfVhNJN9Q86jPcFKTogPLytfKEug&usqp=CAU");
+        allPhotos.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKmJpelOZfVhNJN9Q86jPcFKTogPLytfKEug&usqp=CAU");
 //        a.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKmJpelOZfVhNJN9Q86jPcFKTogPLytfKEug&usqp=CAU");
 
 
-        MainActivityLibrary.initImages(MainActivity.this,a);
-//      MainActivityLibrary.deleteImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKmJpelOZfVhNJN9Q86jPcFKTogPLytfKEug&usqp=CAU",a);
-        MainActivityLibrary.addPhoto("https://www.travelanddestinations.com/wp-content/uploads/2019/10/Ban-Gioc-Detian-Waterfalls-Vietnam-1.jpg",MainActivity.this,a);
-      MainActivityLibrary.deletePhoto("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKmJpelOZfVhNJN9Q86jPcFKTogPLytfKEug&usqp=CAU",a);
-        MainActivityLibrary.openAlbum(MainActivity.this,a);
+
+
+        MainActivityLibrary.initImages(MainActivity.this);
+        MainActivityLibrary.addPhoto("https://www.travelanddestinations.com/wp-content/uploads/2019/10/Ban-Gioc-Detian-Waterfalls-Vietnam-1.jpg",allPhotos);
+        MainActivityLibrary.deletePhoto("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKmJpelOZfVhNJN9Q86jPcFKTogPLytfKEug&usqp=CAU",allPhotos);
+        MainActivityLibrary.openAlbum(MainActivity.this,allPhotos);
 
     }
 }
--- mylibrary/src/main/java/com/example/mylibrary/MainActivityLibrary.java
+++ mylibrary/src/main/java/com/example/mylibrary/MainActivityLibrary.java
@@ -4,45 +4,32 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.graphics.drawable.Drawable;
 import android.os.Handler;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.widget.TableRow;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivityLibrary {
-    public static ImageView activity_main_IMG_1,activity_main_IMG_2,activity_main_IMG_3,activity_main_IMG_4,activity_main_IMG_5
-            ,activity_main_IMG_6,activity_main_IMG_7,activity_main_IMG_8,activity_main_IMG_9,activity_main_IMG_10,activity_main_IMG_11,activity_main_IMG_12,
-                activity_main_IMG_13,activity_main_IMG_14,activity_main_IMG_15;
-    public static TextView title;
     public static View view;
     public static Button activity_main_BTN_back;
     public static ArrayList<Image> arrayOfImages;
     public static AlertDialog dialog= null;
-    public static final int NUM_OF_IMAGES=15;
-    public static ArrayList<ImageView>allImageView;
+    public static LinearLayout linearLayout1;
+    public static LinearLayout linearLayout;
 
-    public static void initImages(Activity activity,ArrayList<String>allImages)  {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+    public static void initImages(Activity activity) {
+
         final LayoutInflater inflater = activity.getLayoutInflater();
         view = inflater.inflate(R.layout.activity_main1, null);
-        builder.setView(view);
-        dialog = builder.create();
-        dialog.show();
-        title = view.findViewById(R.id.title);
-        arrayOfImages = new ArrayList<>();
-        init(allImages);
-        activity_main_BTN_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.dismiss();
-            }
-        });
-
-       openD(allImages.size());
+        linearLayout1 = view.findViewById(R.id.lin);
+        activity_main_BTN_back = view.findViewById(R.id.activity_main_BTN_back);
+        linearLayout1.setGravity(Gravity.CENTER);
     }
 
 
@@ -63,97 +50,59 @@ public class MainActivityLibrary {
 
 
     }
-    public static void addPhoto(String urlToAdd, Activity activity, ArrayList<String>arrayList){
-            if (urlToAdd.isEmpty()) {
-            Toast.makeText( activity,"Error", Toast.LENGTH_SHORT).show();
-            } else {
-                if (arrayList.size()<allImageView.size()) {
-                    arrayList.add(urlToAdd);
-                    init(arrayList);
-                }
-                else{
-                    Toast.makeText( activity,"Delete images before add new", Toast.LENGTH_SHORT).show();
-                }
-        }
+
+    public static void addPhoto(String urlToAdd, ArrayList<String>arrayList){
+        arrayList.add(urlToAdd);
     }
-    public static void openAlbum(Activity activity, ArrayList<String> allImages){
-        refresh();
-        for (int i = 0; i < arrayOfImages.size(); i++) {
+    public static void openAlbum(Activity activity, ArrayList<String> allImages) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+
+        linearLayout1.setOrientation(LinearLayout.VERTICAL);
+        linearLayout1.setGravity(Gravity.CENTER);
+
+        int count = 0;
+        for (int i = 0; i < allImages.size(); i++, count++) {
             String url = allImages.get(i);
+            if (count % 3 == 0) {
+                linearLayout = new LinearLayout(activity);
+                linearLayout1.addView(linearLayout);
+                linearLayout.setGravity(Gravity.RIGHT);
+                linearLayout1.setGravity(Gravity.RIGHT);
+                linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+                linearLayout.setPadding(3, 3, 3, 3);
+            }
             if (url.isEmpty()) {
                 Toast.makeText(activity, "Error", Toast.LENGTH_SHORT).show();
             } else {
-                if(arrayOfImages.get(i).getURL()!="") {
-                    LoadImage loadImage = new LoadImage(arrayOfImages.get(i).getImageView());
-                        arrayOfImages.get(i).getImageView().setBackgroundDrawable(Drawable.createFromPath(url));
-                        loadImage.execute(url);
+                if (allImages.get(i) != "") {
+                    ImageView image = new ImageView(activity);
+                    LoadImage loadImage = new LoadImage(image);
+                    image.setBackgroundDrawable(Drawable.createFromPath(url));
+                    TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(270, 270);
+                    image.setLayoutParams(layoutParams);
+                    linearLayout.addView(image);
+                    loadImage.execute(url);
                 }
             }
         }
-    }
-
-    private static void refresh() {
-        for (int i = 0; i < arrayOfImages.size(); i++)
-            arrayOfImages.get(i).getImageView().setBackgroundResource(R.drawable.rectangle);
-
+        activity_main_BTN_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        builder.setView(view);
+        dialog = builder.create();
+        dialog.show();
+        openD(allImages.size());
     }
 
     public static void deletePhoto(String urlToDelete, ArrayList<String>arrayListURL){
-        for (Image image:arrayOfImages) {
-            if(image.getURL().equals(urlToDelete)) {
+        for (String url:arrayListURL) {
+            if(url.equals(urlToDelete)) {
                 arrayListURL.remove(urlToDelete);
-                init(arrayListURL);
                 return;
             }
         }
     }
-
-
-    private static void init(ArrayList<String>arrayList) {
-        findView(view);
-        if(allImageView!=null) {
-            allImageView.clear();
-            arrayOfImages.clear();
-        }
-        allImageView = new ArrayList<ImageView>();
-        allImageView.add(activity_main_IMG_1);
-        allImageView.add(activity_main_IMG_2);
-        allImageView.add(activity_main_IMG_3);
-        allImageView.add(activity_main_IMG_4);
-        allImageView.add(activity_main_IMG_5);
-        allImageView.add(activity_main_IMG_6);
-        allImageView.add(activity_main_IMG_7);
-        allImageView.add(activity_main_IMG_8);
-        allImageView.add(activity_main_IMG_9);
-        allImageView.add(activity_main_IMG_10);
-        allImageView.add(activity_main_IMG_11);
-        allImageView.add(activity_main_IMG_12);
-        allImageView.add(activity_main_IMG_13);
-        allImageView.add(activity_main_IMG_14);
-        allImageView.add(activity_main_IMG_15);
-
-        for (int j = 0; j < arrayList.size(); j++) {
-                arrayOfImages.add(new Image(allImageView.get(j), arrayList.get(j)));
-        }
-    }
-
-    private static void findView(View view) {
-        activity_main_IMG_1 = view.findViewById(R.id.activity_main_IMG_1);
-        activity_main_IMG_2 = view.findViewById(R.id.activity_main_IMG_2);
-        activity_main_IMG_3 = view.findViewById(R.id.activity_main_IMG_3);
-        activity_main_IMG_4 = view.findViewById(R.id.activity_main_IMG_4);
-        activity_main_IMG_5 = view.findViewById(R.id.activity_main_IMG_5);
-        activity_main_IMG_6 = view.findViewById(R.id.activity_main_IMG_6);
-        activity_main_IMG_7 = view.findViewById(R.id.activity_main_IMG_7);
-        activity_main_IMG_8 = view.findViewById(R.id.activity_main_IMG_8);
-        activity_main_IMG_9 = view.findViewById(R.id.activity_main_IMG_9);
-        activity_main_IMG_10 = view.findViewById(R.id.activity_main_IMG_10);
-        activity_main_IMG_11 = view.findViewById(R.id.activity_main_IMG_11);
-        activity_main_IMG_12 = view.findViewById(R.id.activity_main_IMG_12);
-        activity_main_IMG_13 = view.findViewById(R.id.activity_main_IMG_13);
-        activity_main_IMG_14 = view.findViewById(R.id.activity_main_IMG_14);
-        activity_main_IMG_15 = view.findViewById(R.id.activity_main_IMG_15);
-        activity_main_BTN_back= view.findViewById(R.id.activity_main_BTN_back);
-    }
-
 }
